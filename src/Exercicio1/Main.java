dd679952a0dcc5abbaeb9b5b4891d1e8e979be45
Luis-Gustavo-S-Oliveira <luisgu6666@gmail.com>
2025-11-11 17:16:08 -0300

    commit exercicio1 complet

--- src/Exercicio1/Main.java
+++ src/Exercicio1/Main.java
@@ -1,4 +1,19 @@
 package Exercicio1;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double anosExperiencia;
+        System.out.println("Digite apenas numeros");
+        System.out.print("Quantos anos de experiência você tem com programação? ");
+        anosExperiencia = scan.nextInt();
+        System.out.println("Quantos projetos você já participou?");
+        int projetosParticipados = scan.nextInt();
+
+        if (anosExperiencia < 1 && projetosParticipados < 2) {
+            System.out.println("você é um estagiario");
+        }else System.out.println("você é uma Junior");
+    }
 }
